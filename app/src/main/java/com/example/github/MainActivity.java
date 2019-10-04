0dc8302d6a32df14ace106d948eb0a6e6f43e9b4
Sandra Romeiro <sandrasromeiro76@gmail.com>
2019-10-03 21:26:20 -0300

    FOram colocados botaões de etid text senha

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements view.OnclickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botao = findViewById(R.id.editText);
+        botao = setOnclickListener(this);
     }
 }
